032750297b1c58780c6b2828c5276df4dfccaafb
goroguz <gmgkorohova@gmail.com>
2025-07-09 19:17:41 +0300

    Added Yandex browser support

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,17 +4,34 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
     public static final String CHROMEDRIVER_PATH = "/Users/A13039316/Downloads/chromedriver-mac-arm64/chromedriver";
+    public static final String YANDEX_BINARY_PATH = "/Applications/Yandex.app/Contents/MacOS/Yandex";
+    public static final String YANDEXDRIVER_PATH = "/Users/you/Downloads/yandexdriver";
     public static final String URL = "https://stellarburgers.nomoreparties.site/";
     protected WebDriver driver;
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH);
-        driver = new ChromeDriver();
+        String browser = System.getProperty("browser", "chrome");
+
+        if ("chrome".equalsIgnoreCase(browser)) {
+            System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH);
+            driver = new ChromeDriver();
+        } else if ("yandex".equalsIgnoreCase(browser)) {
+            System.setProperty("webdriver.chrome.driver", YANDEXDRIVER_PATH);
+
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary(YANDEX_BINARY_PATH);
+
+            driver = new ChromeDriver(options);
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
         driver.get(URL);
     }
 
